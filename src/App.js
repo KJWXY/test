c7df6743aa6f17235dadf40ccf82907b50e5a842
KJWXY <emamari96@live.it>
2023-10-06 10:47:42 +0200

    Test changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 
 function App() {
+const test = 'TEST'
+const number = 3
+
   return (
     <div className="App">
       <div className="content">
-        <h1> App Component</h1>
+        <h1>App Component</h1>
+        <h1>{test} {number}</h1>
       </div>
     </div>
   );
 }
 
 export default App;
-/*test comment #2*/
