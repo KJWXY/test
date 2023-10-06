86a427f37dfd52ba8a61dc0ca621d14338b9f24e
KJWXY <emamari96@live.it>
2023-10-06 11:34:03 +0200

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-const test = 'TEST'
-const number = 3
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
-        <h1>{test} {number}</h1>
+        <Home />
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>TEST Website</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/new">New</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
